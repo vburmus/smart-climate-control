729c52d42fc46620edda514ce991a83a49c011c1
maryushp <maryushp@gmail.com>
2024-01-23 00:33:19 +0100

    feat(rooms) connected rooms card and page with logic

--- ui/src/components/RoomCard.tsx
+++ ui/src/components/RoomCard.tsx
@@ -1,22 +1,27 @@
 import React from 'react';
 import {DropletFill, Thermometer} from "react-bootstrap-icons";
+import {Room} from "../utils/types";
 
-const RoomCard = () => {
+type RoomCardProps = {
+    room: Room
+}
+
+const RoomCard = ({room}: RoomCardProps) => {
     return (
         <div className="d-flex flex-column bg-primary rounded-4">
             <div className="p-3">
-                <h2 className="fw-bold text-white text-center">Room #1</h2>
+                <h2 className="fw-bold text-white text-center">Room #{room.id}</h2>
             </div>
             <div className="bg-info p-4 rounded-bottom-4 px-5">
                 <h4 className="fw-bold text-center">Last Update</h4>
                 <div className="d-flex flex-wrap mt-5 gap-4">
                     <div className="d-flex flex-column gap-3 align-items-center px-4">
                         <Thermometer size={35}/>
-                        <h5 className="fw-bold text-center">20.6 °C</h5>
+                        <h5 className="fw-bold text-center">{room.temperature} °C</h5>
                     </div>
                     <div className="d-flex flex-column gap-3 align-items-center px-4">
                         <DropletFill size={35}/>
-                        <h5 className="fw-bold text-center">45 %</h5>
+                        <h5 className="fw-bold text-center">{room.humidity} %</h5>
                     </div>
                 </div>
             </div>
--- ui/src/pages/RoomsPage.tsx
+++ ui/src/pages/RoomsPage.tsx
@@ -1,15 +1,40 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import Loader from "../components/common/Loader";
 import RoomCard from "../components/RoomCard";
+import {findAllRooms} from "../utils/roomsUtils";
+import {Room} from "../utils/types";
+import {isAxiosError} from "axios";
 
 const RoomsPage = () => {
-    const [isLoading, setIsLoading] = useState(false)
+    const [isLoading, setIsLoading] = useState(true)
+    const [rooms, setRooms] = useState<Room[]>()
+    const [error, setError] = useState("")
 
-    return ( isLoading ? <Loader/> :
+    const fetchData = async () => {
+        try {
+            const result = await findAllRooms();
+            setRooms(result.rooms)
+        } catch(e) {
+            if (isAxiosError(e) && e.response) {
+                setError(e.response.data.message)
+            }
+        }
+    };
+
+
+    useEffect(() => {
+        fetchData()
+        setIsLoading(false);
+    }, []);
+
+
+    return ( isLoading ? <Loader/> : error || !rooms ? <h1 className="m-5 fw-bold text-center text-danger">{error}</h1> :
         <div className="d-flex flex-column m-5 gap-5">
             <h1 className="fw-bold text-primary text-center">Your rooms:</h1>
             <div className="d-flex flex-wrap gap-5 justify-content-center">
-                <RoomCard/>
+                {rooms.map((room) => {
+                    return <RoomCard room={room}/>
+                })}
             </div>
         </div>
     );
