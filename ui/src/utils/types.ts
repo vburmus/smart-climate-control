364b85fef970bcec9667381e36abd08edf42d1b1
maryushp <maryushp@gmail.com>
2024-01-23 00:31:28 +0100

    feat(types) provided types

---
+++ ui/src/utils/types.ts
@@ -0,0 +1,16 @@
+export interface Alert {
+    id: number,
+    action: string,
+    cause: string,
+    room: number,
+    date: string
+}
+
+export interface Room {
+    id: number,
+    name: string,
+    preferredTemp?: number,
+    humidity: number,
+    temperature: number,
+    time: string
+}
