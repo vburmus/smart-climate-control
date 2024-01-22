4f9244c6c3cc433d805f4841205eb8c4b02cda77
maryushp <maryushp@gmail.com>
2024-01-22 16:31:52 +0100

    fix(Loader) fixed loader spinner

--- ui/src/components/common/Loader.tsx
+++ ui/src/components/common/Loader.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import "../../styles/Loader.css"
 
 const Loader = () => {
     return (
-        <div className="spinner-border text-primary" role="status">
-            <span className="sr-only"></span>
+        <div className="loader">
+            <div className="spinner-border text-primary" role="status">
+                <span className="sr-only"></span>
+            </div>
         </div>
     );
 };
