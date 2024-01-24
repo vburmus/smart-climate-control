d91fc78d945dda7dda77c50a7ff297cbd03ded52
maryushp <maryushp@gmail.com>
2024-01-24 18:04:11 +0100

    fix(axios) changed api url

--- ui/src/utils/axiosConfig.ts
+++ ui/src/utils/axiosConfig.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://156.17.234.83:8080'
+    baseURL: 'https://156.17.234.83:8443'
 })
 
 export default axiosInstance;
